ae04160329a76332694bccd3067115c5edfba50c
ilia <iliailia@me.com>
2020-04-13 22:38:54 +0200

    firebase added

--- src/Components/PostPage.js
+++ src/Components/PostPage.js
@@ -1,15 +1,36 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../Components/PostPage.css';
 import {Link} from 'react-router-dom';
+import db from './Firebase';
 
 function Post(){
 
+    const [input, setInput] = useState('');
+
     useEffect(()=>{
         // retrieve data from firebase database
-    })
+        console.log('input has changed, ' + input);
+    }, [input])
+
+    function changeInput(e){
+        setInput(e.target.value);
+    }
 
     function postNote(){
         // post data to firebase
+
+        if(input != null){
+            db.collection('notes').add({
+                note: input
+            })
+            .then(()=>{
+                console.log('note saved');
+            })
+            .catch(()=>{
+                console.log('error adding this document');
+            })
+        }
+        setInput('');
     }
 
     return(
@@ -20,7 +41,7 @@ function Post(){
                 </ul>
             </div>
             <div className="col-md-12">
-                <input className="postInput"></input>
+                <input className="postInput" onChange={changeInput} value={input}></input>
             </div>
             <div className="col-md-12 buttonMargin">
                 <Link onClick={postNote} className="postButton">post</Link>
