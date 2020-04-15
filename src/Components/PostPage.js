6bbde2729286d9c98b14a5d01e348aa0433cd8bf
ilia <iliailia@me.com>
2020-04-15 04:07:14 +0200

    post section update

--- src/Components/PostPage.js
+++ src/Components/PostPage.js
@@ -5,12 +5,30 @@ import db from './Firebase';
 
 function Post(){
 
+    const [id, setId] = useState(['']);
+    const [data, setData] = useState(['']);
     const [input, setInput] = useState('');
+    const purpleColor = "#4300FF";
+
+    const dynamicBorderStyle = {
+        borderLeft: "10px solid " + purpleColor
+    }
 
     useEffect(()=>{
-        // retrieve data from firebase database
-        console.log('input has changed, ' + input);
-    }, [input])
+        const firestoreData = [''];
+        const firestoreId = [''];
+        db.collection('notes').get().then((querySnapshot) => {
+            querySnapshot.forEach((queryElement) => {
+                if(queryElement != null){
+                    firestoreData.push(queryElement.data().note)
+                    firestoreId.push(queryElement.id);
+                }
+            })
+            setData(firestoreData);
+            setId(firestoreId);
+        })
+        console.log("function called")
+    },[])
 
     function changeInput(e){
         setInput(e.target.value);
@@ -19,12 +37,15 @@ function Post(){
     function postNote(){
         // post data to firebase
 
-        if(input != null){
+        if(input !== ''){
             db.collection('notes').add({
                 note: input
             })
             .then(()=>{
-                console.log('note saved');
+                setData([
+                    ...data,
+                    input
+                ])
             })
             .catch(()=>{
                 console.log('error adding this document');
@@ -36,15 +57,23 @@ function Post(){
     return(
         <div className="row postRow">
             <div className="col-md-12 postMainWrapper">
-                <ul>
-
-                </ul>
+                <div>
+                 {data === [''] ? <p>no data received</p> : data.map(function (el){
+                     if(el !== ''){
+                     return <div className="noteBox" style={dynamicBorderStyle}>
+                                <p key={id} className="noteBoxP">{el}</p> 
+                                <span className="noteBoxS">march 31, 2020</span>
+                            </div>                   
+                     }
+                     return null;
+                 })}
+                </div>
             </div>
             <div className="col-md-12">
                 <input className="postInput" onChange={changeInput} value={input}></input>
             </div>
             <div className="col-md-12 buttonMargin">
-                <Link onClick={postNote} className="postButton">post</Link>
+                <button onClick={postNote} className="postButton">post</button>
             </div>
             <div className="col-md-12 buttonMargin">
                 <Link to="/" className="backButton">go back</Link>
